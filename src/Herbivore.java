70442adb7061d3db1c2e93b483d5b51e661a723c
nsverum <nsverum@gmail.com>
2023-05-17 19:12:42 +0200

    Changed the Eat method for Herbivore to more universal for herbivores that eat small animals

--- src/Herbivore.java
+++ src/Herbivore.java
@@ -7,20 +7,46 @@ public abstract class Herbivore extends Animal{
         super(energy, isAlive, type);
     }
     public void eat (Location location) {
+        ConcurrentHashMap<Type, CopyOnWriteArrayList<Animal>> animalMap = location.getAnimalMap();
         if (isHungry()) {
             if (location.getPlantVolume() > getType().getMaxEnergy()) {
                 setEnergy((int) (getEnergy() + getType().getMaxEnergy()));
                 location.setPlantVolume((int) (location.getPlantVolume() - getType().getMaxEnergy()));
                // System.out.println("Herbivore " + this + " is eating" + ", Кількість рослин = " + location.getPlantVolume());
             }
-            else if (getEnergy() < 0){
-                setAlive(false);
+            else {
+                int maxValueFromRation = 1;
+                Map.Entry<Type, CopyOnWriteArrayList<Animal>> bestForEat = null;
+                for (Map.Entry<Type, CopyOnWriteArrayList<Animal>> entry : animalMap.entrySet()){
+                    if (!entry.getValue().isEmpty()){
+                        int ValueFromRation = getType().getRation().get(entry.getKey());
+                        if(ValueFromRation > maxValueFromRation) {
+                            maxValueFromRation = ValueFromRation;
+                            bestForEat = entry;
+                        }
+                    }
+                }
+                if (bestForEat != null && !bestForEat.getValue().isEmpty()) {
+                    setEnergy((int) (getEnergy() + bestForEat.getKey().getWeight()));
+                    //  System.out.println("Всеядний " + this + "is eating " + bestForEat.getValue().get(0));
+                    bestForEat.getValue().remove(bestForEat.getValue().size() - 1);
+                    location.setNumAnimalsToDie(location.getNumAnimalsToDie() + 1);
+                }
+                else if (getEnergy() < 0){
+                    setAlive(false);
+                    location.getAnimalMap().values().remove(this);
+                    location.setNumAnimalsToDie(location.getNumAnimalsToDie() + 1);
+                   //   System.out.println("Herbivore " + this + " died of hunger " );
 
-                location.getAnimalMap().values().remove(this);
-             //  System.out.println("Herbivore " + this + " died"+ ", Кількість рослин = " + location.getPlantVolume());
-                location.setNumAnimalsToDie(location.getNumAnimalsToDie() +1);
-
+                }
             }
+//            else if (getEnergy() < 0){
+//                setAlive(false);
+//
+//                location.getAnimalMap().values().remove(this);
+//             //  System.out.println("Herbivore " + this + " died"+ ", Кількість рослин = " + location.getPlantVolume());
+//                location.setNumAnimalsToDie(location.getNumAnimalsToDie() +1);
+//            }
         }
 
     }
